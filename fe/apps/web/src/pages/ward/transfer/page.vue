e9b3b26b150e300a27f379f67775899640d39cd4
Heewook Yoon <hw010603@gmail.com>
2026-07-22 15:50:26 +0900

    [fe]style: 계좌번호 직접 입력 버튼 타이포 조정

--- fe/apps/web/src/pages/ward/transfer/page.vue
+++ fe/apps/web/src/pages/ward/transfer/page.vue
@@ -128,7 +128,7 @@ function selectRecipient(recipient: TransferRecipient) {
     />
 
     <Button
-      class="w-full"
+      class="type-h2 w-full"
       label="계좌 번호 직접 입력하기"
       size="large"
       @click="router.push({ name: 'ward-transfer-account' })"
