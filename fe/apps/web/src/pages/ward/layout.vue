90883b2f48121ccb78d5768c75fc6b90c2028748
Heewook Yoon <hw010603@gmail.com>
2026-08-18 09:41:47 +0900

    [fe]refac: 중복 ForegroundPushNotification 컴포넌트 및 테스트 제거

--- fe/apps/web/src/pages/ward/layout.vue
+++ fe/apps/web/src/pages/ward/layout.vue
@@ -4,7 +4,6 @@ import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import WardBottomNavigation from '@/pages/ward/-components/WardBottomNavigation.vue'
-import ForegroundPushNotification from '@/components/notifications/ForegroundPushNotification.vue'
 import { usePairingStore } from '@/stores/pairing.store'
 
 type WardNavigationValue = 'transfer' | 'home' | 'payment'
@@ -70,7 +69,6 @@ function handleNavigate(value: string) {
             : 'pb-xl'
         "
       >
-        <ForegroundPushNotification />
         <RouterView />
       </main>
 
